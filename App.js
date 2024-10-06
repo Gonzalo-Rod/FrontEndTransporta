0a5e660d4fc2243ffe4a88ad2bc97c448c785fc3
Gonzalo-Rod <gonzalo.rodriguez.g@utec.edu.pe>
2024-10-05 19:23:21 -0500

    Set up project

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from './screens/login';
+import MapScreen from './screens/mapscreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+       <Stack.Navigator screenOptions={{ headerShown: false }}>
+       <Stack.Screen name="Login" component={Login} />
+       <Stack.Screen name="MapScreen" component={MapScreen} />
+       </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ screens/mapscreen.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { View, Text } from "react-native"
+
+import HomeMap from '../components/HomeMap';
+
+const HomeScreen = (props) => {
+    return (
+        <View>
+            <HomeMap />
+
+            {/* Covid Message*/}
+            {/* Bottom Comp*/}
+        </View>
+    );
+}
+
+export default HomeScreen;
